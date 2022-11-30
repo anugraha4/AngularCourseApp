9d3e15c353b0526dc557e0710d43ad7d2b8ee449
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-11-30 16:43:53 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { ViewCourseComponent } from './view-course/view-course.component';
-
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddCourseComponent
+  },
+  {
+    path:"view",
+    component:ViewCourseComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +24,8 @@ import { ViewCourseComponent } from './view-course/view-course.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
